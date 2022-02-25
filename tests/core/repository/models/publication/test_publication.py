aaa3d2349682fce53d90c3f9e1c0a000ae13be78
VICTOR HUGO <victor.oliveira@willbank.com.br>
2022-02-25 16:00:33 -0300

    adding test register user

--- tests/core/repository/models/publication/test_publication.py
+++ tests/core/repository/models/publication/test_publication.py
@@ -6,7 +6,8 @@ class TestPublicationattributes:
         publication = Publication(
             title="testando title",
             content_url="https://www.youtube.com/watch?v=iDJM3HTdjck&ab_channel=30PRAUM",
-            description="testando a descricao"
+            description="testando a descricao",
+            content_level="BEGINNER"
         )
 
         assert publication.title == 'testando title'
--- tests/core/web_apps/users/route_users/test_create_user.py
+++ tests/core/web_apps/users/route_users/test_create_user.py
@@ -1,14 +1,6 @@
-import pytest
-from starlette.testclient import TestClient
-
-from config.config import app
 
 
 class TestCreateUserForm:
-    @pytest.fixture()
-    def client(self):
-        return TestClient(app)
-
     def test_create_user(self, client):
 
         payload = {
@@ -16,4 +8,6 @@ class TestCreateUserForm:
             "email": "test@gmail.com",
             "password": "passtest",
         }
-        result = client.post("/register", data=payload)
+        result = client.post("/register/", data=payload)
+
+        assert result.status_code == 200
